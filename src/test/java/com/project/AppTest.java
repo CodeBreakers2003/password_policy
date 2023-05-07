a0f9426e4abd167e5a691f29a0a5fe051d252e9a
Riane597 <97638682+Riane597@users.noreply.github.com>
2023-05-06 21:13:51 -0300

    Update AppTest.java

--- src/test/java/com/project/AppTest.java
+++ src/test/java/com/project/AppTest.java
@@ -16,13 +16,52 @@ public class AppTest
     // Wachtwoorden moeten minstens één hoofdletter, één kleine letter, één cijfer en één symbool bevatten
     @Test
     public void uppercaseTest() {
-        assertTrue(Check.uppercase("abC"));
-        assertTrue(Check.uppercase("abCdeF"));
+        assertTrue(Check.checkUppercase("Abc"));
+        assertTrue(Check.checkUppercase("abCdeF"));
+        assertFalse(Check.checkUppercase("abcde"));
     }
 
-    // @Test
-    // public void lowercaseTest() {
-    //     assertTrue(Check.lowercase("abc"));
-    //     // assertFalse(Check.lowercase(null));
-    // }
+    @Test
+    public void lowercaseTest() {
+        assertTrue(Check.checkLowercase("abc"));
+        assertTrue(Check.checkLowercase("AbcD"));
+        assertFalse(Check.checkLowercase("ABCD"));
+    }
+
+    @Test
+    public void cijferTest() {
+        assertTrue(Check.checkNumber("dsf97s9d"));
+        assertFalse(Check.checkNumber("sdfds"));
+    }
+    
+    // Wachtwoorden mogen geen usernames bevatten.
+    @Test
+    public void passwordNotContainUsernameTest() {
+        String password = "myusername123";
+        String username = "myusername";
+        assertTrue(Check.passwordNotContainUsername(password, username));
+    
+    }
+
+    @Test
+    public void passwordContainUsernameTest() {
+        String password = "479h8nwHe8uw9p";
+        String username = "myusername";
+        assertFalse(Check.passwordNotContainUsername(password, username));
+    }
+
+    //Wachtwoorden mogen geen namen bevatten.
+    @Test
+    public void passwordNotContainsNameTest() {
+        String password = "JohnDoe123";
+        String name = "sam";
+        assertTrue(Check.passwordNotContainsName(password, name));
+    }
+
+    @Test
+    public void passwordContainsNameTest() {
+        String password = "JohnDoe123";
+        String name = "John";
+        assertFalse(Check.passwordNotContainsName(password, name));
+    }
 }
