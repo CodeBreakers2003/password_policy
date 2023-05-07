68c3f6f8c4f7a826ff51278ebad336bc891f51e4
Riane597 <97638682+Riane597@users.noreply.github.com>
2023-05-06 21:15:18 -0300

    Update Check.java

--- src/main/java/com/project/Check.java
+++ src/main/java/com/project/Check.java
@@ -7,7 +7,7 @@ public class Check {
     }
 
 
-    public static boolean uppercase(String input) {
+    public static boolean checkUppercase(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (Character.isUpperCase(input.charAt(i))) {
                 return true;
@@ -16,8 +16,36 @@ public class Check {
         return false;
     }
 
-    public static boolean lowercase(String input) {
+    public static boolean checkLowercase(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isLowerCase(input.charAt(i))) {
+                return true;
+            }
+        }
         return false;
     }
 
+    public static boolean checkNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean passwordNotContainUsername(String password, String username) {
+        if (password.contains(username)) {
+            return true;
+        }  
+    return false;
+    }
+
+    public static boolean passwordNotContainsName(String password, String name) {
+        if (password.contains(name)) {
+            return false;
+        }  
+    return true;
+    }
 }
