2d3e48a504f18a72a099dd43abc9429ea4de344f
mow <shahrukhc2003@hotmail.com>
2023-05-11 10:56:19 -0300

    added comments

--- src/main/java/com/project/Check.java
+++ src/main/java/com/project/Check.java
@@ -2,11 +2,12 @@ package com.project;
 
 public class Check {
 
+    // Checks the password lenght
     public static boolean checkLength(String password) {
         return password.length() >= 12;
     }
 
-
+    // Checks the password for uppercase characters
     public static boolean checkUppercase(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (Character.isUpperCase(input.charAt(i))) {
@@ -16,6 +17,7 @@ public class Check {
         return false;
     }
 
+    // Checks the password for lowercase characters
     public static boolean checkLowercase(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (Character.isLowerCase(input.charAt(i))) {
@@ -25,6 +27,7 @@ public class Check {
         return false;
     }
 
+    // Checks the password for numbers
     public static boolean checkNumber(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (Character.isDigit(input.charAt(i))) {
@@ -34,7 +37,7 @@ public class Check {
         return false;
     }
 
-
+    // Checks if the password contains the username
     public static boolean passwordNotContainUsername(String password, String username) {
         if (password.contains(username)) {
             return true;
@@ -42,6 +45,7 @@ public class Check {
     return false;
     }
 
+    // Cheks if the password contians the user name
     public static boolean passwordNotContainsName(String password, String name) {
         if (password.contains(name)) {
             return false;
@@ -49,6 +53,7 @@ public class Check {
     return true;
     }
 
+    // Cheks if the password matches a prevous used password
     public static boolean checkForPrevPasw(String hash1, String hash2) {
         return (hash1 == hash2);
     }
